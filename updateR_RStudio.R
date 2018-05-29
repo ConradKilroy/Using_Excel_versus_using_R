fe1db841620694ea6f9b40a4fc34dd063a1d4c48
ConradKilroy <conradkilroy@gmail.com>
2018-05-28 23:07:07 -0700

    Create updateR_RStudio.R

---
+++ updateR_RStudio.R
@@ -0,0 +1,8 @@
+#source:
+#http://bioinfo.umassmed.edu/bootstrappers/bootstrappers-courses/courses/rCourse/Additional_Resources/Updating_R.html
+
+# installing/loading the package:
+if(!require(installr)) {
+  install.packages("installr"); 
+  require(installr)
+} #load / install+load installr
